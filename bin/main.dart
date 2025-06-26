6a02f79547559ac11eea3b64c12afc1dcc342f48
wing72 <xowkddl@gmail.com>
2025-06-26 19:04:51 +0900

    main.dart shopping.dart과 연결

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+import 'package:dart_shopping_mall_code/ShoppingMall.dart';
+
+void main() {
+  final mall = ShoppingMall();
+  mall.hall();
+}
---
+++ lib/Product.dart
@@ -0,0 +1,13 @@
+
+// class Product {
+final
+//   final cart = <String, int>_Product[
+//     '멜론'
+//     '수박'
+//     '딸기'
+//     '바나나'
+//     '사과'
+//   ]
+
+//   }
+
